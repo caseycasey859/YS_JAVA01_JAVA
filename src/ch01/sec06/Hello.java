baabe0bdfd8faef59cb635fea12a81202258788b
casey casey <caseycasey859@gmail.com>
2024-08-07 12:34:28 +0900

    I can do

--- src/ch01/sec06/Hello.java
+++ src/ch01/sec06/Hello.java
@@ -3,7 +3,7 @@ package ch01.sec06;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hi Java07");
+		System.out.println("I can push for github.");
 		
 		// TODO Auto-generated method stub
 
