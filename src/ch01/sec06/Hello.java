d3d40ca34dfd13a91244af102df0a049074a20be
casey casey <caseycasey859@gmail.com>
2024-08-07 12:27:17 +0900

    Java study start

---
+++ src/ch01/sec06/Hello.java
@@ -0,0 +1,12 @@
+package ch01.sec06;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java!!!");
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
